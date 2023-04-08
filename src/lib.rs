d90b29486e7ee781986615e5508cf9eddf61ac24
Turner <mitch@tpfs.io>
2023-04-08 16:54:24 -0700

    Add logic with meaningless test

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,62 @@
+use async_trait::async_trait;
+use naumachia::{
+    ledger_client::LedgerClient,
+    logic::{SCLogic, SCLogicResult},
+    transaction::TxActions,
+};
+
 pub mod scripts;
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct MyLogic;
+
+#[async_trait]
+impl SCLogic for MyLogic {
+    type Endpoints = ();
+    type Lookups = ();
+    type LookupResponses = ();
+    type Datums = ();
+    type Redeemers = ();
+
+    async fn handle_endpoint<Record: LedgerClient<Self::Datums, Self::Redeemers>>(
+        _endpoint: Self::Endpoints,
+        _ledger_client: &Record,
+    ) -> SCLogicResult<TxActions<Self::Datums, Self::Redeemers>> {
+        let empty_actions = TxActions::v2();
+        Ok(empty_actions)
+    }
+
+    async fn lookup<Record: LedgerClient<Self::Datums, Self::Redeemers>>(
+        _query: Self::Lookups,
+        _ledger_client: &Record,
+    ) -> SCLogicResult<Self::LookupResponses> {
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use naumachia::{
+        address::PolicyId,
+        ledger_client::test_ledger_client::TestBackendsBuilder,
+        smart_contract::{SmartContract, SmartContractTrait},
+        Address,
+    };
+
+    #[tokio::test]
+    async fn nothing_test() {
+        let me = Address::from_bech32("addr_test1qrksjmprvgcedgdt6rhg40590vr6exdzdc2hm5wc6pyl9ymkyskmqs55usm57gflrumk9kd63f3ty6r0l2tdfwfm28qs0rurdr").unwrap();
+        let start_amount = 100_000_000;
+        let backend = TestBackendsBuilder::new(&me)
+            .start_output(&me)
+            .with_value(PolicyId::Lovelace, start_amount)
+            .finish_output()
+            .build_in_memory();
+
+        let endpoint = ();
+        let contract = SmartContract::new(&MyLogic, &backend);
+
+        contract.hit_endpoint(endpoint).await.unwrap();
+    }
+}
--- src/scripts.rs
+++ src/scripts.rs
@@ -19,3 +19,22 @@ pub fn get_script() -> ScriptResult<RawPlutusValidator<(), ()>> {
         .map_err(|e| ScriptError::FailedToConstruct(e.to_string()))?;
     Ok(raw_script_validator)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use naumachia::scripts::context::{pub_key_hash_from_address_if_available, ContextBuilder};
+    use naumachia::scripts::ValidatorCode;
+    use naumachia::Address;
+
+    #[test]
+    fn test() {
+        let script = get_script().unwrap();
+
+        let owner = Address::from_bech32("addr_test1qpmtp5t0t5y6cqkaz7rfsyrx7mld77kpvksgkwm0p7en7qum7a589n30e80tclzrrnj8qr4qvzj6al0vpgtnmrkkksnqd8upj0").unwrap();
+
+        let owner_pkh = pub_key_hash_from_address_if_available(&owner).unwrap();
+        let ctx = ContextBuilder::new(owner_pkh).build_spend(&vec![], 0);
+        script.execute((), (), ctx).unwrap();
+    }
+}
